b56b17a159f56964ebe22777b05a0ad0d7f8657a
FTA1984NOW <fernando_andrade00@hotmail.com>
2022-05-12 23:45:04 -0300

    segunda commit no repo!

---
+++ src/main/java/com/pipo/samewithme/samewithme/SamewithmeApplication.java
@@ -0,0 +1,13 @@
+package com.pipo.samewithme.samewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamewithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamewithmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipo/samewithme/samewithme/SamewithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipo.samewithme.samewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamewithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
